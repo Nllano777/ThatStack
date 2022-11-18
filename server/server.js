0ac2e9e2b4f9c8a7ef1d811839f83d52dd667172
Nllano777 <nllbr99@gmail.com>
2022-11-17 21:40:00 -0600

    apollo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const routes = require('./routes');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -25,6 +23,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
