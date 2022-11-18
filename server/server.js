2ac3f115cdafcb9cb692f8bd53b52eaa7c05369a
Nllano777 <nllbr99@gmail.com>
2022-11-17 19:31:14 -0600

    added schemas and routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
